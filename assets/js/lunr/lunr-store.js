648719cd229c6dbaa53fa99b896f103665c0c74e
Dc0deZero <166308193+DcodeZero@users.noreply.github.com>
2024-12-08 15:40:51 +0530

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -155,17 +155,14 @@ var store = [{
         "url": "/goad/GOAD-p1/",
         "teaser": "/assets/images/sample/GOAD/GOAD.webp"
       },{
-        "title": "Game of Active Directory - Part 2",
-        "excerpt":" ","categories": ["GOAD"],
-        "tags": ["GOAD,","HomeLab"],
-        "url": "/goad/GOAD-p2/",
-        "teaser": "/assets/images/sample/GOAD/GOAD.webp"
-      },{
     "title": "About",
     "excerpt":"About Me   Welcome to my corner of the internet! I’m a passionate hacker, Red Team specialist, and penetration tester dedicated to uncovering vulnerabilities and fortifying defenses in the ever-evolving field of cybersecurity.   My Journey   My journey into hacking began with a simple curiosity about how systems work. Over the years, I’ve honed my skills through various avenues:           Red Teaming: I specialize in simulating real-world attacks to test and improve organizational security. My goal is to think like an adversary, uncovering potential weaknesses before they can be exploited.            Penetration Testing: I perform comprehensive security assessments, identifying vulnerabilities in applications and networks. I’m committed to providing actionable insights to enhance security postures.            CTF Competitions: I thrive on challenges and regularly participate in Capture The Flag (CTF) competitions. These events push my skills to the limit and allow me to collaborate with other cybersecurity enthusiasts.            Hack The Box: I actively engage with platforms like Hack The Box, where I tackle real-world hacking scenarios that sharpen my skills and keep me on my toes.       Home Labs   In my spare time, I invest in building and experimenting with home labs. These environments allow me to simulate attacks and defenses, giving me hands-on experience that is crucial for staying ahead in the field. I enjoy sharing insights and strategies with the community, fostering a collaborative learning atmosphere.   Join Me   I believe that the key to a safer digital world lies in continuous learning and collaboration. Join me as I explore the intricacies of cybersecurity, share knowledge, and work towards making the digital landscape a safer place for everyone.    ","url": "http://localhost:4000/about/"
   },{
     "title": "Posts by Category",
     "excerpt":" ","url": "http://localhost:4000/categories/"
+  },{
+    "title": "You really think I've helped you",
+    "excerpt":"Like the work ??   Thank you for visiting my blog and taking the time to explore my work! 💖   If you’ve enjoyed my posts or found them helpful, here’s how you can show your support by subscribing to the blogs.   Why Support Matters   Running this blog takes time and effort, and every bit of support helps me continue doing what I love—sharing insights, stories, and tips with you!   Thank you for being part of this journey. 💫  ","url": "http://localhost:4000/helpedyou/"
   },{
     "title": "IT Posts",
     "excerpt":" ","url": "http://localhost:4000/it-posts/"
